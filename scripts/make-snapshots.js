eb378bd6af48479372eae9917cb3a63e6caad231
Taylor Beseda <tbeseda@gmail.com>
2022-03-22 11:17:11 -0600

    move snapshots to versioned folder

--- scripts/make-snapshots.js
+++ scripts/make-snapshots.js
@@ -1,13 +1,15 @@
 const { exec } = require('child_process')
 const { scripts } = require('../package.json')
 
+const NODE_MAJOR_VERSION = process.versions.node.split('.')[0]
+
 const commands = Object.keys(scripts).filter((key) => key.indexOf('tap-arc:') === 0)
 
 for (const command of commands) {
   const [ name, args = '' ] = command.split(':').slice(1)
 
   exec(
-    `npm run --silent ${command} > test/snapshots/${name}${args}.txt`,
+    `npm run --silent ${command} > test/snapshots/node${NODE_MAJOR_VERSION}/${name}${args}.txt`,
     (_error, _stdout, stderr) => {
       if (stderr) console.log(`Unexpected stderror: ${stderr}`)
       console.log(`Snapped ${name} ${args} with ${command}`)
