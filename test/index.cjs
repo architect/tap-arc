3052b81e8fcea9f5db4f078e5b68de306687c8f3
tbeseda <tbeseda@gmail.com>
2023-09-11 12:36:57 -0600

    playing windarts from a windows machine

--- test/index.cjs
+++ test/index.cjs
@@ -1,14 +1,12 @@
 const { exec } = require('child_process')
 const test = require('tape')
 
-const reporterPath = `${__dirname}/../index.js`
-
 test('exit codes', (t) => {
   const failingTests = [ 'diff', 'mixed', 'simple', 'throws' ]
   for (const ft of failingTests) {
     t.test(`exit(1) "${ft}" | tap-arc`, (t) => {
       exec(
-        `npx tape ${__dirname}/create-${ft}-tap.cjs | ${reporterPath}`,
+        `npx tape ${__dirname}/create-${ft}-tap.cjs | npx tap-arc`,
         (error, stdout, stderr) => {
           t.ok(error, `"${ft}" creates an error`)
           t.notOk(stderr, 'stderror should be empty')
@@ -23,7 +21,7 @@ test('exit codes', (t) => {
   for (const pt of passingTests) {
     t.test(`exit(0) "${pt}" | tap-arc`, (t) => {
       exec(
-        `npx tape ${__dirname}/create-${pt}-tap.cjs | ${reporterPath}`,
+        `npx tape ${__dirname}/create-${pt}-tap.cjs | npx tap-arc`,
         (error, stdout, stderr) => {
           t.notOk(error, 'error should be undefined')
           t.notOk(stderr, 'stderror should be empty')
