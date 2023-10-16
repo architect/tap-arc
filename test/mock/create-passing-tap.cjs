8fb112ec4ff9fec9031454c86f8f567b5807c17e
Taylor Beseda <tbeseda@gmail.com>
2023-10-16 00:22:40 -0600

    use tap-reader

--- test/mock/create-passing-tap.cjs
+++ test/mock/create-passing-tap.cjs
@@ -4,8 +4,8 @@
 const test = require('tape')
 
 test('basic arithmetic', (t) => {
-  t.equal(2 + 3, 5)
-  t.equal(7 * 8 + 9, 65)
+  t.equal(5, 5)
+  t.equal(65, 65)
 
   t.end()
 })
@@ -13,8 +13,8 @@ test('basic arithmetic', (t) => {
 test('deep equality', (t) => {
   t.plan(2)
 
-  t.deepEqual([ 3, 4, 5 ], [ 3, 4, 2 + 3 ])
-  t.deepEqual({ a: 7, b: [ 8, 9 ] }, { a: 3 + 4, b: [ 4 * 2 ].concat(3 * 3) })
+  t.deepEqual([ 3, 4, 5 ], [ 3, 4, 5 ])
+  t.deepEqual({ a: 7, b: [ 8, 9 ] }, { a: 7, b: [ 8 ].concat(9) })
 })
 
 test('comparing booleans', (t) => {
@@ -25,7 +25,7 @@ test('comparing booleans', (t) => {
 
 test('negatives', (t) => {
   t.plan(3)
-  t.notEqual(1 + 2, 5)
+  t.notEqual(3, 5)
   t.notDeepEqual([ 1, 2 ], [ 12 ])
   t.notOk(false)
 })
@@ -49,27 +49,27 @@ test('map with elements', (t) => {
 test('more info', (t) => {
   t.plan(2)
 
-  t.equal(1 + 2, 3, 'basic arithmetic still works')
-  t.ok(3 + 4 > 5, 'inequalities are as we might expect')
+  t.equal(3, 3, 'basic arithmetic still works')
+  t.ok(7 > 5, 'inequalities are as we might expect')
 })
 
 test('asynchronous results', (t) => {
   t.plan(2)
 
-  t.equal(2 + 3, 5)
+  t.equal(5, 5)
 
   setTimeout(function () {
-    t.equal(5 + 5, 10)
+    t.equal(10, 10)
   }, 500)
 })
 
 test('nested', (t) => {
-  t.test((st) => {
+  t.test('a nested test 1', (st) => {
     st.plan(1)
-    st.equal(1 + 2, 3)
+    st.equal(3, 3)
   })
 
-  t.test((st) => {
+  t.test('a nested test 2', (st) => {
     st.plan(1)
     setTimeout(function () {
       st.pass()
